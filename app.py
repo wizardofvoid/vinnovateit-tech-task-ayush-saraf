7ab6d3b12267e8c8e4586be43c4841aee1a01052
wizardofvoid <sarafa736@gmail.com>
2025-07-18 14:11:57 +0530

    Added LLM functionality

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 import streamlit as st
 import pickle
 import numpy as np
+from together import Together
+import os
+
+os.environ["TOGETHER_API_KEY"] = "074b4378fb0c7f824df7c0abbcb452bac9e95109f49283c790269a9a1c6e3db9"
+
+def llama_prompt(attendance, midterm, assignment_avg, quiz_avg, participation, previous, missed, result):
+    if(result==1):
+        keyword = "pass"
+    else:
+        keyword = "fail"
+    
+    client = Together()
+    response = client.chat.completions.create(
+    model = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+    messages=[
+      {
+        "role": "user",
+        "content": f"""The student {keyword}. Their data is:
+    - Attendance: {attendance}%
+    - Midterm: {midterm}
+    - Assignment Avg: {assignment_avg}
+    - Quiz Avg: {quiz_avg}
+    - Participation: {participation}
+    - Previous Score: {previous}
+    - Missed Assignments: {missed}
+
+    Based on this data, what is the best area for them to improve? Give a short explanation."""
+      }
+    ]
+    )
+    return (response.choices[0].message.content)
 
 # Load model
 with open("academic_predictor.pkl", "rb") as f:
@@ -27,3 +58,7 @@ if st.button("Predict"):
     prediction = model.predict(input_data)[0]
     result = "Pass" if prediction == 1 else "Fail"
     st.subheader(f"Prediction: {result}")
+    with st.spinner("Analyzing with LLaMA..."):
+        explaination = llama_prompt(attendance, midterm, assignment_avg, quiz_avg, participation, previous, missed, result)
+        st.markdown("### AI Explanation")
+        st.write(explaination.strip())
